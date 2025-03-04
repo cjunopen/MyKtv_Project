441db762a5bae7842eb1379e68cd1ea38283e43f
chenjun <853364235@qq.com>
2025-03-04 21:06:12 +0800

    新建videoplayer 和 home

---
+++ videoplayer/src/main/java/com/cj/videoplayer/VideoPlayerLib.java
@@ -0,0 +1,20 @@
+package com.cj.videoplayer;
+
+import xyz.doikki.videoplayer.ijk.IjkPlayerFactory;
+import xyz.doikki.videoplayer.player.AndroidMediaPlayerFactory;
+import xyz.doikki.videoplayer.player.VideoViewConfig;
+import xyz.doikki.videoplayer.player.VideoViewManager;
+
+/**
+ * @Description:
+ * @Author: CJ
+ * @CreateDate: 2025/3/3 21:52
+ */
+public class VideoPlayerLib {
+    public static void init(){
+        VideoViewManager.setConfig(VideoViewConfig.newBuilder()
+                //使用使用IjkPlayer解码
+                .setPlayerFactory(IjkPlayerFactory.create())
+                .build());
+    }
+}
